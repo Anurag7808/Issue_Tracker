6c11e156b11b8d8e59f975a0af7f70a3ed6efd51
Anurag7808 <88648321+Anurag7808@users.noreply.github.com>
2023-08-23 11:40:02 +0530

    Delete routes directory

--- routes/issues.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const issueController = require('../controllers/issue_controller');
-const passport = require('passport');
-
-
-// router.post('/createIssue',passport.checkAuthentication,issueController.createIssue);
-
-module.exports = router;
